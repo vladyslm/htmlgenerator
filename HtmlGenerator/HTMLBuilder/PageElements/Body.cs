0438215cc67d2f13dae7f8dec54c3e5781fc9bee
Vladyslav <vladyslav@pop-os.localdomain>
2022-06-01 21:09:11 +0200

    add: Body implementation

---
+++ HtmlGenerator/HTMLBuilder/PageElements/Body.cs
@@ -0,0 +1,25 @@
+namespace HtmlGenerator.HTMLBuilder.PageElements;
+using HtmlGenerator.Components.Tag;
+
+public class Body : IPageElement
+{
+    private List<IHtmlElement> _htmlElements = new();
+
+    public List<IHtmlElement> HtmlElements => _htmlElements;
+
+    public void AddHtmlElement(params IHtmlElement[] component)
+    {
+        _htmlElements.AddRange(component);
+    }
+
+    public string Prepare()
+    {
+        var components = new List<string>();
+        foreach (var htmlElement in _htmlElements)
+        {
+            components.Add(htmlElement.Build());
+        }
+        var joinedComponents = String.Join("\n", components);
+        return String.Format($"<body>\n{joinedComponents}\n</body>");
+    }
+}
