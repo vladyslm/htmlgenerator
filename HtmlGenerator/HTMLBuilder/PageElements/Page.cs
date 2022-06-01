e9bc3212576417a25a4c321c499c15af1ec3275a
Vladyslav <vladyslav@pop-os.localdomain>
2022-06-01 21:09:34 +0200

    add: Page implementation

---
+++ HtmlGenerator/HTMLBuilder/PageElements/Page.cs
@@ -0,0 +1,30 @@
+namespace HtmlGenerator.HTMLBuilder.PageElements;
+
+public class Page : IPageElement
+{
+    
+    private string _lang = String.Empty;
+
+    public string Language
+    {
+        get => _lang;
+        set => _lang = String.Format($" lang=\"{value}\"");
+    }
+
+    public Head Head { get; }
+
+    public Body Body { get; }
+
+    public Page(Head headElement, Body bodyElement)
+    {
+        Head = headElement;
+        Body = bodyElement;
+    }
+
+    public string Prepare()
+    {
+        var body = Body.Prepare();
+        var head = Head.Prepare();
+        return String.Format($"<html{_lang}>\n{head}\n{body}\n</html>");
+    }
+}
