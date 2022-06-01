c75051c4643883a46c52f64940e7d54842ae4890
Vladyslav <vladyslav@pop-os.localdomain>
2022-06-01 21:08:48 +0200

    add: Head implementation

---
+++ HtmlGenerator/HTMLBuilder/PageElements/Head.cs
@@ -0,0 +1,32 @@
+namespace HtmlGenerator.HTMLBuilder.PageElements;
+
+public class Head : IPageElement
+{
+    private string _style = String.Empty;
+    private string _title = String.Empty;
+
+    public string Title
+    {
+        get => _title;
+        set => _title = String.Format($"\n\t<title>{value}</title>");
+    }
+
+    public string Style
+    {
+        get => _style;
+        set => _style = String.Format($"\n\t<link rel=\"stylesheet\" href=\"{value}\">");
+    }
+
+    public string Prepare()
+    {
+        if (
+            _title != String.Empty ||
+            _style != String.Empty
+        )
+        {
+            return String.Format($"<head>{_title}{_style}\n</head>");
+        }
+
+        return String.Empty;
+    }
+}
