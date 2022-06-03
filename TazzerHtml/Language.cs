28e0d12aa4f3f1f5c122595d678710254cafbe13
Vladyslav <vladyslav@pop-os.localdomain>
2022-06-03 22:53:14 +0200

    add: Language const values

---
+++ TazzerHtml/Language.cs
@@ -0,0 +1,17 @@
+namespace TazzerHtml;
+
+/// <summary>
+/// Represents const language values.
+/// </summary>
+public static class Language
+{
+    /// <summary>
+    /// English = "en"
+    /// </summary>
+    public const string English = "en";
+    
+    /// <summary>
+    /// Ukrainian = "uk"
+    /// </summary>
+    public const string Ukrainian = "uk";
+}
